a1baf70fd442997fe5d9ff093de4cdd77377fc1b
akasharkxx <akash.jaiswar41@gmail.com>
2021-01-15 16:20:12 +0530

    Initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject playerVehicle;
+    [SerializeField] private Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = playerVehicle.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveSpeed, turnSpeed;
+    private float horizontalInput, forwardInput;
+
+    private void Start()
+    {
+        moveSpeed = 15.0f;
+        turnSpeed = 25.0f;
+    }
+
+    private void Update()
+    {
+        //Get Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * forwardInput);
+        //Steer vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
